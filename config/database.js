2ad5fbc604f60460aee2183c0291e7504bbcf01b
meri-xx <meri.leeworthy@gmail.com>
2021-04-11 19:27:17 +1000

    change process.env to env()

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const parse = require("pg-connection-string").parse;
 
 module.exports = ({ env }) => {
   if (env("NODE_ENV") === "production") {
-    const config = parse(process.env.DATABASE_URL);
+    const config = parse(env(DATABASE_URL));
     return {
       defaultConnection: "default",
       connections: {
