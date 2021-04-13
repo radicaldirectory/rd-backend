fc4c734558519e694e199a0322c8d35ac6b13653
meri-xx <meri.leeworthy@gmail.com>
2021-04-13 19:07:37 +1000

    try passing SSL cert into config

--- config/database.js
+++ config/database.js
@@ -12,6 +12,9 @@ module.exports = ({ env }) => {
             database: env("DATABASE_NAME", "strapi"),
             username: env("DATABASE_USERNAME", "strapi"),
             password: env("DATABASE_PASSWORD", "strapi"),
+            ssl: {
+              ca: env("DATABASE_SSL_CERT", ""),
+            },
           },
           options: {
             ssl: true,
