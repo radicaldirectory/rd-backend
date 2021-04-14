e9828a3fec5916b062dba4a846e9a88055eaeb72
meri-xx <meri.leeworthy@gmail.com>
2021-04-14 17:46:27 +1000

    nah never mind, uploads config goes in plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -10,4 +10,15 @@ module.exports = ({ env }) => ({
       defaultReplyTo: "mail@radical.directory",
     },
   },
+  upload: {
+    provider: "do",
+    providerOptions: {
+      key: env("DO_SPACE_ACCESS_KEY", ""),
+      secret: env("DO_SPACE_SECRET_KEY", ""),
+      endpoint: env("DO_SPACE_ENDPOINT", ""),
+      space: env("DO_SPACE_BUCKET", ""),
+      directory: env("DO_SPACE_DIRECTORY", ""),
+      cdn: env("DO_SPACE_CDN", ""),
+    },
+  },
 });
