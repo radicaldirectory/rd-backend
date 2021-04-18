e09979df80b0dbb8bffa18557031dced18f7b39b
meri-xx <meri.leeworthy@gmail.com>
2021-04-18 14:36:25 +1000

    env conditional

--- config/plugins.js
+++ config/plugins.js
@@ -1,24 +1,28 @@
 module.exports = ({ env }) => ({
-  email: {
-    provider: "mailgun",
-    providerOptions: {
-      apiKey: env("MAILGUN_API_KEY"),
-      domain: env("MAILGUN_DOMAIN"), //Required if you have an account with multiple domains
+  ...(process.env.EMAIL_ENABLE && {
+    email: {
+      provider: "mailgun",
+      providerOptions: {
+        apiKey: env("MAILGUN_API_KEY", ""),
+        domain: env("MAILGUN_DOMAIN", ""), //Required if you have an account with multiple domains
+      },
+      settings: {
+        defaultFrom: "mail@radical.directory",
+        defaultReplyTo: "mail@radical.directory",
+      },
     },
-    settings: {
-      defaultFrom: "mail@radical.directory",
-      defaultReplyTo: "mail@radical.directory",
+  }),
+  ...(process.env.UPLOAD_ENABLE && {
+    upload: {
+      provider: "do",
+      providerOptions: {
+        key: env("DO_SPACE_ACCESS_KEY"),
+        secret: env("DO_SPACE_SECRET_KEY", ""),
+        endpoint: env("DO_SPACE_ENDPOINT", ""),
+        space: env("DO_SPACE_BUCKET", ""),
+        directory: env("DO_SPACE_DIRECTORY", ""),
+        cdn: env("DO_SPACE_CDN", ""),
+      },
     },
-  },
-  upload: {
-    provider: "do",
-    providerOptions: {
-      key: env("DO_SPACE_ACCESS_KEY", ""),
-      secret: env("DO_SPACE_SECRET_KEY", ""),
-      endpoint: env("DO_SPACE_ENDPOINT", ""),
-      space: env("DO_SPACE_BUCKET", ""),
-      directory: env("DO_SPACE_DIRECTORY", ""),
-      cdn: env("DO_SPACE_CDN", ""),
-    },
-  },
+  }),
 });
